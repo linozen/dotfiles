27efbf4fca497baae78a76e728385a61ce588d6b
Linus Sehn <linus@fsfe.org>
2022-01-30 13:05:46 +0100

    added gitconfig

--- users/modules/symlinks.nix
+++ users/modules/symlinks.nix
@@ -6,5 +6,9 @@
     ".config/qt5ct/qt5ct.conf".source = ./qt5ct.conf;
     ".ssh/config".source = ./ssh/client/config;
     ".config/Element/config.json".source = ./element/config.json;
+    # TODO Setup git using Nix
+    # See: https://nixos.wiki/wiki/Git
+    ".gitconfig".source = ./gitconfig/.gitconfig;
+    ".gitconfig-fsfe".source = ./gitconfig/.gitconfig-fsfe;
   };
 }
