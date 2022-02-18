72c7bcea0444d00b2833fc14d370b1865e50c92d
Linus Sehn <linus@fsfe.org>
2022-02-18 08:58:00 +0100

    reorder keys

--- users/modules/gpg.nix
+++ users/modules/gpg.nix
@@ -6,6 +6,22 @@
     enable = true;
     publicKeys = [
       {
+        source = ./gpg/fsfe_erik.key;
+        trust = "marginal";
+      }
+      {
+        source = ./gpg/fsfe_heiki.key;
+        trust = "marginal";
+      }
+      {
+        source = ./gpg/fsfe_jackie.key;
+        trust = "marginal";
+      }
+      {
+        source = ./gpg/fsfe_linda.key;
+        trust = "marginal";
+      }
+      {
         source = ./gpg/fsfe_max.key;
         trust = "marginal";
       }
@@ -15,22 +31,6 @@
       }
       {
         source = ./gpg/fsfe_patrick.key;
-        trust = "marginal";
-      }
-      {
-        source = ./gpg/fsfe_heiki.key;
-        trust = "marginal";
-      }
-      {
-        source = ./gpg/fsfe_heiki.key;
-        trust = "marginal";
-      }
-      {
-        source = ./gpg/fsfe_linda.key;
-        trust = "marginal";
-      }
-      {
-        source = ./gpg/fsfe_jackie.key;
         trust = "marginal";
       }
     ];
