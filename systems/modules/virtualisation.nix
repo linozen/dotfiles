368b0abeb760553d834c287b83b39f9f5830d8b0
Linus Sehn <linus@fsfe.org>
2022-02-18 08:58:55 +0100

    remove nvidia-docker

--- systems/modules/virtualisation.nix
+++ systems/modules/virtualisation.nix
@@ -4,14 +4,9 @@
   # Enable libvirtd
   virtualisation.libvirtd.enable = true;
 
-  # Needed for nvidia-docker
-  hardware.opengl.driSupport32Bit = true;
-
   # Enable docker
   virtualisation.docker = {
     enable = true;
-    enableNvidia = true;
     autoPrune.enable = true;
   };
-
 }
